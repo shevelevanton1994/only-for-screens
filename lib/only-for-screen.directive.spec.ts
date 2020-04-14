ce6314f3c699ed5dab241b5e5b204814638b2954
antonio <shevelevanton1994@gmail.com>
2020-04-14 12:56:15 +0300

    clean up repo

--- lib/only-for-screen.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { OnlyForScreenDirective } from './only-for-screen.directive';
-
-describe('OnlyForScreenDirective', () => {
-    it('should create an instance', () => {
-        const directive = new OnlyForScreenDirective();
-        expect(directive).toBeTruthy();
-    });
-});
