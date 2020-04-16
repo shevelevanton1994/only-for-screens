7211db674baad223cc803d50d90d9b523472156e
antonio <shevelevanton1994@gmail.com>
2020-04-16 14:37:14 +0300

    unsubscribe resize event on directive destroy

--- projects/only-for-screen/src/lib/only-for-screen.directive.ts
+++ projects/only-for-screen/src/lib/only-for-screen.directive.ts
@@ -1,50 +1,57 @@
 import {
-  Directive,
-  TemplateRef,
-  ViewContainerRef,
-  Input,
-  OnInit,
+    Directive,
+    TemplateRef,
+    ViewContainerRef,
+    Input,
+    OnInit,
+    OnDestroy,
 } from '@angular/core';
-import { fromEvent } from 'rxjs';
+import { fromEvent, Subject } from 'rxjs';
 import { throttleTime, distinctUntilChanged } from 'rxjs/operators';
 @Directive({
-  // tslint:disable-next-line: directive-selector
-  selector: '[onlyForScreen]',
-  providers: [{ provide: Window, useValue: window }],
+    // tslint:disable-next-line: directive-selector
+    selector: '[onlyForScreen]',
+    providers: [{ provide: Window, useValue: window }],
 })
-export class OnlyForScreenDirective implements OnInit {
-  @Input() onlyForScreen;
-  private resize = fromEvent(window, 'resize');
-  private MOBILE = 641;
-  private TABLET = 1025;
+export class OnlyForScreenDirective implements OnInit, OnDestroy {
+    @Input() onlyForScreen;
+    private resize$ = fromEvent(window, 'resize');
+    private unsubscribe$ = new Subject<boolean>();
+    private MOBILE = 641;
+    private TABLET = 1025;
 
-  constructor(
-      private templateRef: TemplateRef<any>,
-      private viewContainer: ViewContainerRef,
-  ) {
-      this.resize
-          .pipe(throttleTime(250), distinctUntilChanged())
-          .subscribe(() => {
-              this.updateOnResize();
-          });
-  }
-  ngOnInit(): void {
-      this.updateOnResize();
-  }
-  actualScreenDimention(): string {
-      if (window.innerWidth < this.MOBILE) {
-          return 'mobile';
-      }
-      if (window.innerWidth < this.TABLET) {
-          return 'tablet';
-      }
-      return 'desktop';
-  }
+    constructor(
+        private templateRef: TemplateRef<any>,
+        private viewContainer: ViewContainerRef,
+    ) {
+        this.resize$
+            .pipe(throttleTime(250), distinctUntilChanged())
+            .subscribe(() => {
+                this.updateOnResize();
+            });
+    }
 
-  updateOnResize() {
-      this.viewContainer.clear();
-      if (this.actualScreenDimention().includes(this.onlyForScreen)) {
-          this.viewContainer.createEmbeddedView(this.templateRef);
-      }
-  }
+    ngOnInit(): void {
+        this.updateOnResize();
+    }
+    actualScreenDimention(): string {
+        if (window.innerWidth < this.MOBILE) {
+            return 'mobile';
+        }
+        if (window.innerWidth < this.TABLET) {
+            return 'tablet';
+        }
+        return 'desktop';
+    }
+
+    updateOnResize() {
+        this.viewContainer.clear();
+        if (this.actualScreenDimention().includes(this.onlyForScreen)) {
+            this.viewContainer.createEmbeddedView(this.templateRef);
+        }
+    }
+    ngOnDestroy(): void {
+        this.unsubscribe$.next(true);
+        this.unsubscribe$.complete();
+    }
 }
